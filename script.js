d6ae067f3adf2ebc4e884c6cdb5305f03f07b23b
I-m-Vaibhav06 <vaibhav19062001@gmail.com>
2025-05-20 07:18:44 +0530

    upcode

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-console.log(b);
 
+/*
+console.log(x);
 let a=10;
  
 var b= 100;
 
-//console.log(a);  */
+console.log(a);
+referencer Error not defined.
+*/
 
-   // referencer Error not defined.
+
+/*  BLOCK SCOPE 
+if(true){
+   var a=20;
+   console.log(a); // 20
+}*/
+
+{
+   var a=10;
+   let b=20;
+   const c=30; 
+}
