98fd5ae9011138482d551ad9abe2593a6ed90d8d
I-m-Vaibhav06 <vaibhav19062001@gmail.com>
2025-05-22 19:23:40 +0530

    pushcode

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-/*
+
 console.log(x);
 let a=10;
  
 var b= 100;
 
-console.log(a);
-referencer Error not defined.
-*/
+console.log(a); //referencer Error not defined.
 
 
-/*  BLOCK SCOPE */
+
+/*  BLOCK SCOPE 
 if(true){
    var a=20;
    console.log(a); // 20
@@ -50,7 +49,7 @@ console.log(c); // 305 */
 /* Shadowing allows us to use the same variable name in different scopes without conflict.
 In the above example, the variable 'a' is shadowed in the block scope, but 'b' and 'c' are not shadowed because they are declared with 'let' and 'const', which have block scope.   */
 
-/* Shadowing alse behaves similar in functionss as well. */
+/* Shadowing alse behaves similar in functionss as well. 
 const c=100;
 function x(){
    const c=30;
@@ -58,9 +57,9 @@ function x(){
    console.log(c); // 30
 }
 x();
-console.log(c); // 100 
+console.log(c); // 100 */
 
-/*Illegal shadowing */
+/*Illegal shadowing 
  var p=10;
  var b=100;
   function x(){     
@@ -75,5 +74,27 @@ const a=10;
       console.log(a); // 1000
    }
    console.log(a); // 100
-}
+} */
 
+function stringMultiplication(str1, str2){
+   console.log(str1.length);
+   console.log(str2.length);
+
+   for(let i=0; i<str1.length; i++)
+      console.log(str1.charAt(i));
+   
+   console.log(str1.concat("  ").concat(str2));
+
+   let newstring = `My First "" '' \ \\\'""'Name is ${str1} and last name is ${str2}`;
+
+   console.log(newstring);
+   console.log(newstring.trim());
+   console.log(newstring.toUpperCase());
+   console.log(newstring.toLowerCase());
+
+   let sub = str1.substring(0, 2);
+
+   //console.log(str1.match());
+   console.log(sub);
+}
+stringMultiplication("John", "Doe");
